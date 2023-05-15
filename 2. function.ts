5be1adf58b88baf2251bc74863e99a197446c99e
Alexis <lexisgasa3007@gmail.com>
2023-05-15 09:52:56 +0200

    funciones

--- 2. function.ts
+++ 2. function.ts
@@ -27,3 +27,25 @@ let square = (x: number): number => {
 let sumOfSquares = (x: number, y: number): number => {
     return  square(x) + square(y) ;
 }
+
+// crea una funcion anonima que reciba un literal y devuelva su longitad
+
+let lenghtOfString = (string: string): number => {
+    return string.length;
+}
+
+// crea una funcion anonima 2 numeros argumento y devuelva el mayor
+
+let maxNumber = (x: number, y: number): number =>  x > y ? x: y;
+
+// funcion coja numero y si es par devuelva true
+
+let isEven = (x: number): boolean => x % 2 === 0 ? true : false;
+
+// funcion principal args num1 y num2, dentro retorna una func. anonima que retorne suma
+
+let primeraFuncion: (num1: number, num2:number) => () => number = (num1, num2) => {
+    return (): number => { 
+        return num1+num2 
+    }
+}
